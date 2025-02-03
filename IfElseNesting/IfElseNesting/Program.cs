90339ace46266acfe8758a1aa01ad215391867bc
Opilane TTHK <opilane@tthk.ee>
2025-02-03 15:12:04 +0200

    init

---
+++ IfElseNesting/IfElseNesting/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace IfElseNesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number Control!");
+
+            int number = 201;
+
+            //kontrollitakse kas arv on suurem 189-st
+            if (number > 189)
+            {
+                //kui vasta is tingimustele, siis näitab konsoolis seda arvu
+                Console.WriteLine("Number is bigger");
+
+                //if ja else nestimine
+                if(number >= 190 && number <= 200)
+                {
+                    Console.WriteLine("Number is between 190 to 200");
+                }
+                else
+                {
+                    Console.WriteLine("Number is bigger than 200");
+                }
+            }
+            else
+            {
+                Console.WriteLine("NUmber is smaller");
+            }
+        }
+    }
+}
