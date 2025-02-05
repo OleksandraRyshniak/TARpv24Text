56d7de57e7a9aecad0ad28a1624cca6c5344d44a
Opilane TTHK <opilane@tthk.ee>
2025-02-05 15:04:20 +0200

    unit

---
+++ CoinCalculator/CoinCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CoinCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Raha ümber maa!");
+
+            //vaja arvutada, mitu kahe eurost münti läheb ümber maa
+            // maa ümbermõõt tuleb välja arvutada raadiuse abil
+            double earthR = 6378000000;
+            double CD = 25.75; //mm
+            double aroundEarth = 2 * Math.PI * earthR;
+
+            Console.WriteLine("Maa ümbermõõt: " + aroundEarth);
+
+            //nüüd arvutame müntide arvu
+            double result = aroundEarth / CD;
+
+            Console.WriteLine("Ümber maa mahub nii " +
+                "palju kahe eurosid " + result);
+
+        }
+    }
+}
