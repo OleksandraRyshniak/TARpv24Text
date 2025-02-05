90d41f7753afe0241e10fce0b0fba74490a3d714
Opilane TTHK <opilane@tthk.ee>
2025-02-05 16:48:22 +0200

    unit

---
+++ KohvikSwitch/KohvikSwitch/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace KohvikSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fantaasia kohvik!");
+            Console.WriteLine("\n\nMenüü (kohvi valik)\n\n");
+            Console.WriteLine("1. Väike tass"); //1.5
+            Console.WriteLine("2. Keskmine tass"); //2.5
+            Console.WriteLine("3. Suur tass"); // 3
+            Console.WriteLine("\n\nPalun tee valik numbriga: ");
+
+            byte coffeSize = byte.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            switch (coffeSize)
+            {
+                case 1:
+                    price = price + 1.5;
+                    break;
+                case 2:
+                    price = price += 2.5;
+                    break;
+                case 3:
+                    price = price + 3;
+                    break;
+                default:
+                    Console.WriteLine("Ei teinud valikut");
+                    break;
+            }
+            if (price != 0)
+            {
+                Console.WriteLine("Tuleb maksta {0:f2}", price);
+                Console.ReadKey();
+            }
+        }
+    }
+}
