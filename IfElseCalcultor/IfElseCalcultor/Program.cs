95ff0006015810583a95780d103a3ea20f1dbc57
Opilane TTHK <opilane@tthk.ee>
2025-02-10 13:09:42 +0200

    unit

---
+++ IfElseCalcultor/IfElseCalcultor/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace IfElseCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //teha kalkulaator ja kasutada if ning else
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insert first number and pess enter:");
+            float firstNr = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insert first number and pess enter: ");
+            float secondNr = float.Parse(Console.ReadLine());
+            Console.WriteLine("Insert calculation type by number");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            int calculationtype = int.Parse(Console.ReadLine());
+            float result = 0;
+            if (calculationtype == 1)
+            {
+                result = firstNr + secondNr;
+                Console.WriteLine("Answer: ");
+                Console.WriteLine(firstNr + "+" + secondNr + "=" + result);
+            }
+            else
+            if (calculationtype == 2)
+            {
+                result = firstNr - secondNr;
+                Console.WriteLine("Answer: ");
+                Console.WriteLine(firstNr + "-" + secondNr + "=" + result);
+            }
+            else
+            if (calculationtype == 3)
+            {
+                result = firstNr * secondNr;
+                Console.WriteLine("Answer: ");
+                Console.WriteLine(firstNr + "*" + secondNr + "=" + result);
+            }
+            else
+            if (calculationtype == 4)
+            {
+                result = firstNr / secondNr;
+                Console.WriteLine("Answer: ");
+                Console.WriteLine(firstNr + "/" + secondNr + "=" + result);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
